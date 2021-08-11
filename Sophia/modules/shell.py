58218813a348b029ad711087e9a1c397f7e479b2
TOBOT UPDATE <83446207+Aaditya318@users.noreply.github.com>
2021-08-11 11:57:30 +0530

    Update shell.py

--- Sophia/modules/shell.py
+++ Sophia/modules/shell.py
@@ -45,6 +45,6 @@ def shell(update: Update, context: CallbackContext):
 
 SHELL_HANDLER = CommandHandler(["sh"], shell)
 dispatcher.add_handler(SHELL_HANDLER)
-__mod_name__ = "💞Shell💞"
+__mod_name__ = "Shell🐚"
 __command_list__ = ["sh"]
 __handlers__ = [SHELL_HANDLER]

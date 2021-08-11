8199e7cd984d9ca54bf3044b40a3ba8adb001c68
TOBOT UPDATE <83446207+Aaditya318@users.noreply.github.com>
2021-08-11 11:49:47 +0530

    Update speedtest.py

--- Sophia/modules/speedtest.py
+++ Sophia/modules/speedtest.py
@@ -59,6 +59,6 @@ SPEED_TEST_CALLBACKHANDLER = CallbackQueryHandler(
 dispatcher.add_handler(SPEED_TEST_HANDLER)
 dispatcher.add_handler(SPEED_TEST_CALLBACKHANDLER)
 
-__mod_name__ = "💞SpeedTest💞"
+__mod_name__ = "SpeedTest☄️"
 __command_list__ = ["speedtest"]
 __handlers__ = [SPEED_TEST_HANDLER, SPEED_TEST_CALLBACKHANDLER]
